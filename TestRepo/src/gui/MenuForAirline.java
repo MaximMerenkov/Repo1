f11682086a31d8c27bded8dd3af56e89d568eb3a
Max <Max@LAPTOP-UIKHULFQ.attlocal.net>
2019-03-28 19:54:10 -0400

    first commit

---
+++ TestRepo/src/gui/MenuForAirline.java
@@ -0,0 +1,37 @@
+package gui;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MenuForAirline extends Application implements EventHandler<ActionEvent>{
+	Button button;
+  
+  public static void main(String[] args) { 
+    launch(args);
+  }
+  
+@Override
+  public void start(Stage primaryStage) throws Exception{
+	primaryStage.setTitle("Online Airline reservation");
+	button = new Button();
+	button.setText("Click here ");
+	button.setOnAction(this);
+	StackPane layout = new StackPane();
+	layout.getChildren().add(button);
+	Scene scene = new Scene(layout,300,250);
+	primaryStage.setScene(scene);
+	primaryStage.show();
+  }
+@Override
+   public void handle(ActionEvent event) {
+	if(event.getSource()==button) {
+		System.out.println("Click here button activated");
+	}
+	   
+   }
+  
+}
